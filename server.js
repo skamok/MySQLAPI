34d12e38872e76ba79093e9114d85021a0ee7b3d
skamok <sk_amok@mail.ru>
2018-10-17 20:39:19 +0300

    rebuild

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 const port = 3000;
 
 app.use('/public', express.static('public'));
+//app.use(express.static('public')); чтоб работало в index.html меняем путь к my.css на css/my.css
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/', 'index.html');
