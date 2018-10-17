f155ba665bea73a75e0dbc89184d6ef53486eb5d
skamok <sk_amok@mail.ru>
2018-10-17 16:02:24 +0300

    build

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
-app.get('/my.css', function(req, res) {
-	res.sendFile(__dirname+'/', 'my.css');
-});
+app.use('/public', express.static('public'));
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/', 'index.html');
 });
